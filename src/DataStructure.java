9ef0d7e57ddd3e4838ad9fc4c88fd32f8a5d6d92
Irakli Gikashvili <i.gikashvili@gmail.com>
2022-07-15 16:05:39 +0400

    Removed main method from the Data Structure Test

--- src/DataStructure.java
+++ src/DataStructure.java
@@ -3,35 +3,28 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public class DataStructure {
-
-    public static void main(String[] args) {
-
-    }
-
     ArrayList<Integer> array;
     HashMap<Integer, Integer> hashMap;
 
 
-//Constructor
+    //Constructor
     public DataStructure() {
         array = new ArrayList<>();
         hashMap = new HashMap<>();
     }
 
-//    Method
+    //    Method
     public void remove(int x) {
         Integer index = hashMap.get(x);
-        if(index == null) return;
+        if (index == null) return;
         hashMap.remove(x);
 
         int size = array.size();
-        Integer last = array.get(size-1);
-        Collections.swap(array, index, size-1);
-        array.remove(size-1);
+        Integer last = array.get(size - 1);
+        Collections.swap(array, index, size - 1);
+        array.remove(size - 1);
         hashMap.put(last, index);
 
     }
-
-
 
 }
